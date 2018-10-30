40636d8a2bb6dd681e2ccf8d40613e2c5724fd40
SANJAY K SAI <shachsan@hotmail.com>
2018-10-30 20:34:53 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,13 +1,8 @@
 require 'pry'
 
 def my_find(collection)
-  result = []
   collection.each do |ele|
     return ele if yield(ele)
-    # result << ele if yield(ele)
-    # if result
-    #   return result
-    # end
   end
   return nil
 end
