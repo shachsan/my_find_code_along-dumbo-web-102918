e7927fa86c21c2d523f5c7ac3bd1e66260f29bbe
SANJAY K SAI <shachsan@hotmail.com>
2018-10-30 20:22:32 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   result = nil
   collection.each do |ele|
-    result = yield(ele) if block_given
+    result = yield(ele) if block_given?
   end
   result
 end
